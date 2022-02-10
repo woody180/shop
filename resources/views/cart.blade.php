55206b7583e0924cead0293281bb767967d68b2c
woody <woody180@rocketmail.com>
2022-02-10 18:18:37 +0400

    Fix cart view

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -3,10 +3,17 @@
 
 @section('content')
 
-<table class="uk-table uk-table-striped">
+@if (request()->session()->get('message'))
+<div class="uk-alert-primary" uk-alert>
+    <a class="uk-alert-close" uk-close></a>
+    <p>{{ request()->session()->get('message') }}</p>
+</div>
+@endif
+
+<table class="uk-table uk-table-striped uk-table-middle">
     <thead>
         <tr>
-            <th>კალათა (2 პროდუქტი)</th>
+            <th class="uk-width-medium">კალათა ({{ $cartItems->count() }} პროდუქტი)</th>
             <th></th>
             <th></th>
         </tr>
@@ -18,7 +25,15 @@
         <tr>
             <td><a href="{{ url('products/'.$item->product->id) }}">{{ $item->product->title }} ({{ $item->quantity }})</a></td>
             <td>{{ $item->quantity * $item->product->price }} $</td>
-            <td><a href="#">ყიდვა</a></td>
+            <td>
+                <form class="uk-margin-remove uk-padding-remove uk-flex uk-flex-right" method="POST" action="{{ url('checkout/' . $item->id) }}">
+                    @csrf
+                    @method('PUT')
+                    <input type="hidden" name="checkedout" value="1" />
+                    
+                    <button class="uk-button uk-button-primary uk-button-small uk-margin-remove">ყიდვა</button>
+                </form>
+            </td>
         </tr>
         @endforeach
         
